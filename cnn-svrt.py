2760d7e70c1a93cd122f1857cc6f6393a6b549a8
Francois Fleuret <francois@fleuret.org>
2017-06-14 23:01:41 +0200

    Added the header.

--- cnn-svrt.py
+++ cnn-svrt.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python-for-pytorch
 
+#  svrt is the ``Synthetic Visual Reasoning Test'', an image
+#  generator for evaluating classification performance of machine
+#  learning systems, humans and primates.
+#
+#  Copyright (c) 2017 Idiap Research Institute, http://www.idiap.ch/
+#  Written by Francois Fleuret <francois.fleuret@idiap.ch>
+#
+#  This file is part of svrt.
+#
+#  svrt is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License version 3 as
+#  published by the Free Software Foundation.
+#
+#  svrt is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with selector.  If not, see <http://www.gnu.org/licenses/>.
+
 import time
 
 import torch
@@ -11,10 +32,9 @@ from torch import nn
 from torch.nn import functional as fn
 from torchvision import datasets, transforms, utils
 
-from _ext import svrt
+import svrt
 
 ######################################################################
-# The data
 
 def generate_set(p, n):
     target = torch.LongTensor(n).bernoulli_(0.5)
