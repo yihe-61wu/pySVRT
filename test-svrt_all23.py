a82be9a5916a00eecee4c0c71ddea642caa425b8
Scott Lowe <scott.code.lowe@gmail.com>
2018-04-13 17:27:56 +0100

    ENH: Add example generator for all 23 problems

---
+++ test-svrt_all23.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+#  svrt is the ``Synthetic Visual Reasoning Test'', an image
+#  generator for evaluating classification performance of machine
+#  learning systems, humans and primates.
+#
+#  Copyright (c) 2017 Idiap Research Institute, http://www.idiap.ch/
+#  Written by Francois Fleuret <francois.fleuret@idiap.ch>
+#
+#  This file is part of svrt.
+#
+#  svrt is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License version 3 as
+#  published by the Free Software Foundation.
+#
+#  svrt is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with svrt.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+
+import torch
+import torchvision
+
+from torch import optim
+from torch import FloatTensor as Tensor
+from torch.autograd import Variable
+from torch import nn
+from torch.nn import functional as fn
+
+from torchvision import datasets, transforms, utils
+
+import svrt
+
+labels = torch.LongTensor(32).zero_()
+labels.narrow(0, 0, labels.size(0)//2).fill_(1)
+
+for problem in range(1, 24):
+
+    x = svrt.generate_vignettes(problem, labels)
+
+    #print('compression factor {:f}'.format(x.storage().size() / svrt.compress(x.storage()).size()))
+
+    x = x.view(x.size(0), 1, x.size(1), x.size(2))
+
+    x.div_(255)
+
+    torchvision.utils.save_image(x, 'example' + str(problem) + '.png')
+
+print('Wrote example pngs')
