dd447bea4eb58e38d2af0c03864ecdb4509f0e81
Scott Lowe <scott.code.lowe@gmail.com>
2018-05-10 19:16:54 +0100

    MAINT: Classic parsed output has shape indexing from 1

--- svrt/parse.py
+++ svrt/parse.py
@@ -34,7 +34,10 @@ def parse_vignette_to_string_classic(
     for i_shape in range(nb_shapes):
         shape_list[i_shape][3] %= 2 * math.pi
         this_shape_str = "Shape({:.0f},{:.0f},{:.0f},{:f})".format(
-            *shape_list[i_shape, (0, 1, 2, 4)])
+            shape_list[i_shape, 0],
+            shape_list[i_shape, 1],
+            shape_list[i_shape, 2] + 1,
+            shape_list[i_shape, 4])
         shape_reps.append(this_shape_str)
     out = ','.join(shape_reps)
     out += "\n"
